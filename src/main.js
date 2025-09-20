ab0cfbe93a467d7c5d23c0baf1be30e490c43899
Mugisha Elie <iradukunda2k24@gmail.com>
2025-09-20 09:51:19 +0200

    `Added new features and styles to index.html and output.css, updated input.css, and modified existing styles in output.css`

---
+++ src/main.js
@@ -0,0 +1,14 @@
+const navbar = document.getElementById("nav-bar");
+
+const stickyAfter = 500;
+
+const body = document.body;
+
+// window.addEventListener("scroll", ()=>{
+//     if(window.scrollY >= stickyAfter){
+//         navbar.classList.add("sticky");
+//         navbar.classList.add("bg-blue-600");
+//         navbar.classList.add("mx-0");
+//         navbar.classList.remove("transparent");
+//     }
+// });
